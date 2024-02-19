a52292b3c20cef3ea0787994fb216d6a7f267de6
rizalkalam <rizalkalam78@gmail.com>
2024-02-19 12:10:37 +0700

    [RK]create filter product catalog

---
+++ katalog.js
@@ -0,0 +1,288 @@
+const data_katalog = [
+    {
+      link: "/Detail Produk/katalog1.html",
+      img: "./Assets/Katalog/katalog1.jpg",
+      name: "Katalog 1",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog2.html",
+      img: "./Assets/Katalog/katalog2.jpg",
+      name: "Katalog 2",
+      category: "Paper Bag",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog3.html",
+      img: "./Assets/Katalog/katalog3.jpg",
+      name: "Katalog 3",
+      category: "Paper Bag",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog4.html",
+      img: "./Assets/Katalog/katalog4.jpg",
+      name: "Katalog 4",
+      category: "Paper Bag",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog5.html",
+      img: "./Assets/Katalog/katalog5.jpg",
+      name: "Katalog 5",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog6.html",
+      img: "./Assets/Katalog/katalog6.jpg",
+      name: "Katalog 6",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog7.html",
+      img: "./Assets/Katalog/katalog7.jpg",
+      name: "Katalog 7",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog8.html",
+      img: "./Assets/Katalog/katalog8.jpg",
+      name: "Katalog 8",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog9.html",
+      img: "./Assets/Katalog/katalog9.jpg",
+      name: "Katalog 9",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog10.html",
+      img: "./Assets/Katalog/katalog10.jpg",
+      name: "Katalog 10",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog11.html",
+      img: "./Assets/Katalog/katalog11.jpg",
+      name: "Katalog 11",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog12.html",
+      img: "./Assets/Katalog/katalog12.jpg",
+      name: "Katalog 12",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog13.html",
+      img: "./Assets/Katalog/kataloog13.jpg",
+      name: "Katalog 13",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog14.html",
+      img: "./Assets/Katalog/katalog14.jpg",
+      name: "Katalog 14",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog15.html",
+      img: "./Assets/Katalog/katalog15.jpg",
+      name: "Katalog 15",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog16.html",
+      img: "./Assets/Katalog/katalog16.jpg",
+      name: "Katalog 16",
+      category: "Tas Spunbond Serut",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog17.html",
+      img: "./Assets/Katalog/katalog17.jpg",
+      name: "Katalog 17",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog18.html",
+      img: "./Assets/Katalog/katalog18.jpg",
+      name: "Katalog 18",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog19.html",
+      img: "./Assets/Katalog/katalog19.jpg",
+      name: "Katalog 19",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog20.html",
+      img: "./Assets/Katalog/katalog20.jpg",
+      name: "Katalog 20",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog21.html",
+      img: "./Assets/Katalog/katalog21.jpg",
+      name: "Katalog 21",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog22.html",
+      img: "./Assets/Katalog/katalog22.jpg",
+      name: "Katalog 22",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog23.html",
+      img: "./Assets/Katalog/katalog23.jpg",
+      name: "Katalog 23",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog24.html",
+      img: "./Assets/Katalog/katalog24.jpg",
+      name: "Katalog 24",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog25.html",
+      img: "./Assets/Katalog/katalog25.jpg",
+      name: "Katalog 25",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog26.html",
+      img: "./Assets/Katalog/katalog26.jpg",
+      name: "Katalog 26",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog27.html",
+      img: "./Assets/Katalog/katalog27.jpg",
+      name: "Katalog 27",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog28.html",
+      img: "./Assets/Katalog/katalog28.jpg",
+      name: "Katalog 28",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog29.html",
+      img: "./Assets/Katalog/katalog29.jpg",
+      name: "Katalog 29",
+      category: "Tas Spunbond",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog30.html",
+      img: "./Assets/Katalog/katalog30.jpg",
+      name: "Katalog 30",
+      category: "Tas Spunbond Serut",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog31.html",
+      img: "./Assets/Katalog/katalog31.jpg",
+      name: "Katalog 31",
+      category: "Tas Spunbond Serut",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+    {
+      link: "/Detail Produk/katalog32.html",
+      img: "./Assets/Katalog/katalog32.jpg",
+      name: "Katalog 32",
+      category: "Tas Spunbond Serut",
+      size: "Ukuran : 20cm X 20cm",
+      price: "Rp 2.000/pcs"
+    },
+  ];
+
+  const filterSelect = document.getElementById("filter-katalog");
+  const katalogDataContainer = document.getElementById("katalog-list");
+  
+  filterSelect.addEventListener("change", function () {
+    const selectedKatalog = filterSelect.value;
+    filterKatalogs(selectedKatalog);
+  });
+
+  function filterKatalogs(selectedKatalog) {
+    const filteredKatalogs = data_katalog.filter((katalog) => {
+        return selectedKatalog === "Semua" || katalog.category === selectedKatalog;
+      });
+
+      const fillKatalog = filteredKatalogs.map(
+        (data) => 
+        `<a href=${data.link}>
+            <div class="card-product">
+                <img src=${data.img} alt="Produk Nailil Barokah" class="image-product">
+                <p class="name-product">${data.name}</p>
+                <p class="size-product">${data.size}</p>
+                <p class="price-product">${data.price}</p>
+            </div>
+        </a>`
+      );
+
+      katalogDataContainer.innerHTML = fillKatalog.join("")
+  }
+
+  filterKatalogs("Semua")
--- logic.js
+++ logic.js
@@ -1,3 +1,106 @@
+let data_product = [
+  {
+    link: "/Detail Produk/katalog1.html",
+    img: "./Assets/Katalog/katalog1.jpg",
+    name: "Katalog 1",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog2.html",
+    img: "./Assets/Katalog/katalog2.jpg",
+    name: "Katalog 2",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog3.html",
+    img: "./Assets/Katalog/katalog3.jpg",
+    name: "Katalog 3",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog4.html",
+    img: "./Assets/Katalog/katalog4.jpg",
+    name: "Katalog 4",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog5.html",
+    img: "./Assets/Katalog/katalog5.jpg",
+    name: "Katalog 5",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog6",
+    img: "./Assets/Katalog/katalog6.jpg",
+    name: "Katalog 6",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog7.html",
+    img: "./Assets/Katalog/katalog7.jpg",
+    name: "Katalog 7",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog8.html",
+    img: "./Assets/Katalog/katalog8.jpg",
+    name: "Katalog 8",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog9.html",
+    img: "./Assets/Katalog/katalog9.jpg",
+    name: "Katalog 9",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog10.html",
+    img: "./Assets/Katalog/katalog10.jpg",
+    name: "Katalog 10",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "/Detail Produk/katalog11.html",
+    img: "./Assets/Katalog/katalog11.jpg",
+    name: "Katalog 11",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "Detail Produk/katalog12.html",
+    img: "./Assets/Katalog/katalog12.jpg",
+    name: "Katalog 12",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+];
+
+const productDataContainer = document.getElementById("product-list");
+
+const fillProduct = data_product.map(
+  (data) => 
+  `<a href=${data.link}>
+      <div class="card-product">
+          <img src=${data.img} alt="Produk Nailil Barokah" class="image-product">
+          <p class="name-product">${data.name}</p>
+          <p class="size-product">${data.size}</p>
+          <p class="price-product">${data.price}</p>
+      </div>
+    </a>`
+);
+
+productDataContainer.innerHTML = fillProduct.join("");
+
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration) {
      let obj = document.getElementById(id),
@@ -48,36 +151,36 @@ function toggleFAQ(card) {
   }
 }
 
-// search
-const filterSelect = document.getElementById("filter-produk");
-const productContainer = document.getElementById("productCar");
+// // search
+// const filterSelect = document.getElementById("filter-produk");
+// const productContainer = document.getElementById("productCar");
 
-filterSelect.addEventListener("change", function () {
-  const selectedProduct = filterSelect.value;
-  filterProducts(selectedProduct);
-});
+// filterSelect.addEventListener("change", function () {
+//   const selectedProduct = filterSelect.value;
+//   filterProducts(selectedProduct);
+// });
 
-function filterProducts(selectedProduct) {
-  const filteredProducts = data_product.filter((product) => {
-    return selectedProduct === "all" || product.type === selectedProduct;
-  });
+// function filterProducts(selectedProduct) {
+//   const filteredProducts = data_product.filter((product) => {
+//     return selectedProduct === "all" || product.type === selectedProduct;
+//   });
 
-  const fillProduct = filteredProducts.map(
-    (data) => `
-        <div class="card-product-car" onclick="window.location.href = '/detail-${data.type}';">
-          <img src="${data.image}" alt="${data.alt}" class="img-car" />
-          <p class="text-type-car">${data.title}</p>
-          <p class="car-price">Mulai : ${data.price}</p>
-          <div class="title-type-car">
-            <iconify-icon icon="mdi:car-outline" class="icon" width="24px"></iconify-icon>
-            <p>${data.desc}</p>
-          </div>
-        </div>
-      `
-  );
+//   const fillProduct = filteredProducts.map(
+//     (data) => `
+//         <div class="card-product-car" onclick="window.location.href = '/detail-${data.type}';">
+//           <img src="${data.image}" alt="${data.alt}" class="img-car" />
+//           <p class="text-type-car">${data.title}</p>
+//           <p class="car-price">Mulai : ${data.price}</p>
+//           <div class="title-type-car">
+//             <iconify-icon icon="mdi:car-outline" class="icon" width="24px"></iconify-icon>
+//             <p>${data.desc}</p>
+//           </div>
+//         </div>
+//       `
+//   );
 
-  productContainer.innerHTML = fillProduct.join("");
-}
+//   productContainer.innerHTML = fillProduct.join("");
+// }
 
-// Inisialisasi dengan menampilkan semua produk saat halaman dimuat.
-filterProducts("all");
+// // Inisialisasi dengan menampilkan semua produk saat halaman dimuat.
+// filterProducts("all");
